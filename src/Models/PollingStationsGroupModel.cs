e6c89f63e379985787343baeaa16a689f700cc72
Ion Dormenco <idormenco@gmail.com>
2020-11-09 13:49:20 +0200

    Return distance to polling station

--- src/Models/PollingStationsGroupModel.cs
+++ src/Models/PollingStationsGroupModel.cs
@@ -4,6 +4,8 @@
     {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public double Distance { get; set; }
+
         public PollingStationModel[] PollingStations { get; set; }
     }
 }
